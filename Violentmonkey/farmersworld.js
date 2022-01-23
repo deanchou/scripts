10fe1e425fe70ec8dd08bbca38dec779c03478c9
dean <dean.chou@aliyun.com>
2022-01-23 21:58:15 +0800

    修正 页面载入慢 导致脚本死锁

--- Violentmonkey/farmersworld.js
+++ Violentmonkey/farmersworld.js
@@ -4,7 +4,7 @@
 // @namespace       zmn
 // @match           https://play.farmersworld.io/
 // @grant           none
-// @version         2022.1.0.6
+// @version         2022.1.0.7
 // @author          Dean.Chou
 // @run-at          document-end
 // @downloadURL     https://raw.githubusercontent.com/deanchou/scripts/master/Violentmonkey/farmersworld.js
@@ -37,85 +37,88 @@
 
 async function autoRobot() {
     console.log('检查')
-
-    // 登录
-    let btnLogin = document.getElementsByClassName('login-button');
-    if (btnLogin.length > 0) {
-        btnLogin[0].click();
-        console.log('点击登录');
-        await sleep(5000);
-        clickWallet();
-        start(15000);
-        return;
-    }
-
-    // 检查体力
-    const currentFish = Math.floor(
-        +document.querySelectorAll(".resource-number")[2].innerText
-    );
-    const [currentEnergy, maxEnergy] = document
-        .querySelectorAll(".resource-number")[3]
-        .textContent.split("/")
-        .map(Number);
-
-    console.log("体力", currentEnergy, maxEnergy, currentFish);
-    if (maxEnergy - currentEnergy > 100 && currentFish > 1) {
-        const countEnergyClicks = Math.min(
-            currentFish,
-            Math.floor((maxEnergy - currentEnergy) / 5)
-        );
-
-        if (countEnergyClicks > 0) {
-            document.querySelector(".resource-energy img").click();
-            await sleep(2000);
-
-            for (let i = 0; i++ < countEnergyClicks;) {
-                document
-                    .querySelector(".image-button[alt='Plus Icon']")
-                    .click();
-                await sleep(200);
-            }
-            document.querySelector(".modal-wrapper .plain-button").click();
+    try {
+        // 登录
+        let btnLogin = document.getElementsByClassName('login-button');
+        if (btnLogin.length > 0) {
+            btnLogin[0].click();
+            console.log('点击登录');
             await sleep(5000);
+            clickWallet();
+            start(15000);
+            return;
         }
-    }
 
-    // 工作
-    let imgTools = document.getElementsByClassName('carousel__img--item');
-    for (let i = 0; i < imgTools.length; i++) {
-        imgTools[i].click();
-        await sleep(500);
+        // 检查体力
+        const resourceNumber = document.querySelectorAll(".resource-number");
+        if (resourceNumber.length > 3) {
+            const currentFish = Math.floor(resourceNumber[2].innerText);
+            const [currentEnergy, maxEnergy] = resourceNumber[3]
+                .textContent.split("/")
+                .map(Number);
 
-        // 修理
-        const buttonRepair = document.querySelectorAll(
-            ".info-section .plain-button"
-        )[1];
-        if (buttonRepair) {
-            const quality = eval(
-                document.querySelector(".card-number").innerText
-            );
-            console.log('耐久', quality);
-            if (
-                ![...buttonRepair.classList].includes("disabled") &&
-                quality < 0.5
-            ) {
-                buttonRepair.click();
-                console.log('修理');
-                await sleep(5000);
+            console.log("体力", currentEnergy, maxEnergy, currentFish);
+            if (maxEnergy - currentEnergy > 100 && currentFish > 1) {
+                const countEnergyClicks = Math.min(
+                    currentFish,
+                    Math.floor((maxEnergy - currentEnergy) / 5)
+                );
+
+                if (countEnergyClicks > 0) {
+                    document.querySelector(".resource-energy img").click();
+                    await sleep(2000);
+
+                    for (let i = 0; i++ < countEnergyClicks;) {
+                        document
+                            .querySelector(".image-button[alt='Plus Icon']")
+                            .click();
+                        await sleep(200);
+                    }
+                    document.querySelector(".modal-wrapper .plain-button").click();
+                    await sleep(5000);
+                }
             }
         }
 
-        let times = document.getElementsByClassName('card-container--time');
-        if (times.length > 0) console.log(times[0].innerHTML);
-        let btnWorks = document.getElementsByClassName('plain-button semi-short');
-        for (let j = 0; j < btnWorks.length; j++) {
-            if (btnWorks[j].innerHTML == 'Mine' || btnWorks[j].innerHTML == 'Claim') {
-                btnWorks[j].click();
-                console.log('开始生产');
-                await sleep(5000);
+        // 工作
+        let imgTools = document.getElementsByClassName('carousel__img--item');
+        for (let i = 0; i < imgTools.length; i++) {
+            imgTools[i].click();
+            await sleep(500);
+
+            // 修理
+            const buttonRepair = document.querySelectorAll(
+                ".info-section .plain-button"
+            )[1];
+            if (buttonRepair) {
+                const quality = eval(
+                    document.querySelector(".card-number").innerText
+                );
+                console.log('耐久', quality);
+                if (
+                    ![...buttonRepair.classList].includes("disabled") &&
+                    quality < 0.5
+                ) {
+                    buttonRepair.click();
+                    console.log('修理');
+                    await sleep(5000);
+                }
             }
+
+            let times = document.getElementsByClassName('card-container--time');
+            if (times.length > 0) console.log(times[0].innerHTML);
+            let btnWorks = document.getElementsByClassName('plain-button semi-short');
+            for (let j = 0; j < btnWorks.length; j++) {
+                if (btnWorks[j].innerHTML == 'Mine' || btnWorks[j].innerHTML == 'Claim') {
+                    btnWorks[j].click();
+                    console.log('开始生产');
+                    await sleep(5000);
+                }
+            }
+            await sleep(500);
         }
-        await sleep(500);
+    } catch (e) {
+        console.log(e);
     }
     start(1000);
 }
